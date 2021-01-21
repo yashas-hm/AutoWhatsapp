83a9046a18b80b0f133c58a5ae782ebc6c2a6b9a
yashas-hm <yashashm@gmail.com>
2021-01-21 16:34:21 +0530

    notification listener implemented

--- app/src/main/java/com/yashas/autowhatsapp/MainActivity.kt
+++ app/src/main/java/com/yashas/autowhatsapp/MainActivity.kt
@@ -1,11 +1,35 @@
 package com.yashas.autowhatsapp
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.NotificationManager
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
 import android.os.Bundle
+import android.provider.Settings
+import androidx.appcompat.app.AppCompatActivity
+import androidx.localbroadcastmanager.content.LocalBroadcastManager
+
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val notificationManager =
+            applicationContext.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        if (!notificationManager.isNotificationPolicyAccessGranted) {
+            startActivity(Intent(Settings.ACTION_NOTIFICATION_LISTENER_SETTINGS))
+        }
+        LocalBroadcastManager.getInstance(this).registerReceiver(onNotice, IntentFilter("Msg"))
+    }
+
+    private val onNotice: BroadcastReceiver = object : BroadcastReceiver() {
+        override fun onReceive(context: Context, intent: Intent) {
+            val packageName = intent.getStringExtra("package")
+            val titleData = intent.getStringExtra("title")
+            val textData = intent.getStringExtra("text")
+
+        }
     }
 }
---
+++ app/src/main/java/com/yashas/autowhatsapp/NotificationListener.kt
@@ -0,0 +1,42 @@
+package com.yashas.autowhatsapp
+
+import android.content.Context
+import android.content.Intent
+import android.service.notification.NotificationListenerService
+import android.service.notification.StatusBarNotification
+import androidx.localbroadcastmanager.content.LocalBroadcastManager
+
+
+class NotificationListener : NotificationListenerService() {
+
+    @Suppress("PrivatePropertyName")
+    private val TAG = this.javaClass.simpleName
+    lateinit var context: Context
+    private var titleData = ""
+    private var textData = ""
+
+    override fun onCreate() {
+        super.onCreate()
+        context = applicationContext
+    }
+
+    override fun onNotificationPosted(sbn: StatusBarNotification) {
+        val packageName = sbn.packageName
+        val extras = sbn.notification.extras
+        if(extras.getString("android.title")!=null){
+            titleData = extras.getString("android.title").toString()
+        }
+
+        if(extras.getCharSequence("android.text")!=null){
+            textData = extras.getCharSequence("android.text").toString()
+        }
+
+        println(titleData)
+
+        val msg = Intent("Msg")
+        msg.putExtra("package", packageName)
+        msg.putExtra("title", titleData)
+        msg.putExtra("text", textData)
+        LocalBroadcastManager.getInstance(context).sendBroadcast(msg)
+    }
+}
